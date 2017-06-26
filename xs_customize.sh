e37cdc0376a5f56ce2cd76c2e765dfd6e2af3527
antonseitz <aseitz@puckit.de>
2017-06-26 13:23:42 +0200

    Update xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 
-# activate Base Repo
+# isntall mc and xinted 
+
+yum --enablerepo=base -y install mc xinetd
 
 
-# install mc
+# load modules for ipmi
 
-# activate ipmi-modules on boot
+modprobe ipmi_si
+modprobe ipmi_devintf
+
+# activate ipmi-modules permanently on host boot
 
 chmod +x /etc/sysconfig/modules/ipmi_si.modules
 chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
@@ -14,14 +19,37 @@ chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
 echo "modprobe ipmi_si" > /etc/sysconfig/modules/ipmi_si.modules
 echo "modprobe ipmi_devintf" > /etc/sysconfig/modules/ipmi_devintf.modules
 
-modprobe ipmi_si
-modprobe ipmi_devintf
 
-# install xinetd ?!
-
-/etc/init.d/xinetd start
 
 # test ipmi sensors
 
+ipmitool sensor
+
+# install check_mk
+
+
+scp root@192.168.1.207:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
+scp root@192.168.1.207:/omd/versions/default/share/check_mk/agents/xinetd.conf /etc/xinetd.d/check_mk
+
+#mv check_mk_agent.linux /usr/bin/check_mk_agent
+#mv xinetd.conf /etc/xinetd.d/check_mk  
+
+# start xinetd 
+
+systemctl enable xinetd.service
+systemctl start xinetd.service
+
+
 # make firewall exceptions
+
+
+# mkdir Backup mount
+mkdir /snapshots
+
+#UUID findet man unter /dev/disk/by-uuid 
+echo "UUID=9750ef5c-807d-406c-bc20-9f7012b24ea1 /snapshots ext4 defaults,noauto 0 2" >> /etc/fstab
+
+mount /snapshots
+
+
 
