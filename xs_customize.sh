c9732dd9d810595a832b7d2359075b50e32582ce
antonseitz <aseitz@puckit.de>
2017-06-26 12:04:10 +0200

    Update xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -8,9 +8,18 @@
 
 # activate ipmi-modules on boot
 
+chmod +x /etc/sysconfig/modules/ipmi_si.modules
+chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
+
+echo "modprobe ipmi_si" > /etc/sysconfig/modules/ipmi_si.modules
+echo "modprobe ipmi_devintf" > /etc/sysconfig/modules/ipmi_devintf.modules
+
+modprobe ipmi_si
+modprobe ipmi_devintf
 
 # install xinetd ?!
 
+/etc/init.d/xinetd start
 
 # test ipmi sensors
 
