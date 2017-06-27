2690bfde01b1bbe3d43c502ae4685e51fc5d4733
antonseitz <aseitz@puckit.de>
2017-06-27 09:45:28 +0200

    Update xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -34,28 +34,43 @@ read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 # install check_mk
 
-
-scp root@192.168.1.207:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
-scp root@192.168.1.207:/omd/versions/default/share/check_mk/agents/xinetd.conf /etc/xinetd.d/check_mk
+read -rsp $'Enter IP of Check_mk host : ' IP
+scp root@$IP:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
+scp root@$IP:/omd/versions/default/share/check_mk/agents/xinetd.conf /etc/xinetd.d/check_mk
 
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 
 # start xinetd 
 
-ls
+systemctl enable xinetd.service
 systemctl start xinetd.service
+
+netstat -lnp | grep 6556
+
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-
-
 
 # make firewall exceptions
-#lokkit -p 6556:tcp  --update
-EDIT /etc/sysconfig/iptables
+echo "Add into /etc/sysconfig/iptables : \n"
+echo " -A RH-Firewall-1-INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 6556 -j ACCEPT "
 
-service iptables restart
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
+nano /etc/sysconfig/iptables
+
+service iptables restart
+service iptables reload 
+read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
+
+telnet localhost 6556
+read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
+
+ifconfig 
+read -rsp $'Enter IP of this host : ' IP
+
+telnet $IP 6556
+
+read -rsp $'Should we make mount point for /snapshots or   to cancel ? (CTRL-C)...\n' -n1
 
 
 # mkdir Backup mount
