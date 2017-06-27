b3b333252c4d67c0dcdc8cf50eff9269394514af
antonseitz <aseitz@puckit.de>
2017-06-27 11:11:52 +0200

    Update xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -23,13 +23,13 @@ chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
 
 echo "modprobe ipmi_si" > /etc/sysconfig/modules/ipmi_si.modules
 echo "modprobe ipmi_devintf" > /etc/sysconfig/modules/ipmi_devintf.modules
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 # test ipmi sensors
 
 ipmitool sensor
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 fi
@@ -40,17 +40,17 @@ read -rsp $'Is the OMD Host reachable per ssh ? [y/n] ?\n' -n1 OMDIP
 if [ $OMDIP == "y" ]; then
 
 # install check_mk
-echo.
+echo
 read -rsp $'Enter IP of Check_mk host : ' IP
 scp root@$IP:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
 scp root@$IP:/omd/versions/default/share/check_mk/agents/xinetd.conf /etc/xinetd.d/check_mk
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 else
-echo.
+echo
 echo "So, you can use script push_check_mk_agent on OMD HOst to push on this machine!"
-echo.
+echo
 fi
 
 # start xinetd 
@@ -59,13 +59,13 @@ systemctl enable xinetd.service
 systemctl start xinetd.service
 
 netstat -lnp | grep 6556
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 # make firewall exceptions
 echo "Add into /etc/sysconfig/iptables : \n"
 echo " -A RH-Firewall-1-INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 6556 -j ACCEPT "
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 nano /etc/sysconfig/iptables
@@ -75,7 +75,7 @@ service iptables reload
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 telnet localhost 6556
-echo.
+echo
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
 
 ifconfig 
@@ -83,7 +83,7 @@ read -rsp $'Enter IP of this host : ' IP
 
 telnet $IP 6556
 
-echo.
+echo
 read -rsp $'Should we make mount point for /snapshots or   to cancel ? (CTRL-C)...\n' -n1
 
 
