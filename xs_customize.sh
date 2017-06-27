dc749b458c2c19159a0fdb3548f61087f1c8c10f
Anton Seitz <aseitz@puckit.de>
2017-06-27 11:53:16 +0200

    Anleitung für Download 	modified:   xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 
-# You can download and install this script per git fetch oder wget:
+# You can download and install this script per git fetch 
+
+# yum --enablerepo=base -y install git
+# git clone https://github.com/antonseitz/xs_customize.git
+
+# oder wget:  (Raw needs sometimes some minutes to offer the newest commit..=)
 
  # rm -f xs_customize.sh && wget --no-cache https://raw.githubusercontent.com/antonseitz/xs_customize/master/xs_customize.sh && chmod u+x xs_customize.sh
 
 echo "Installing mc and xinted per yum\n"
 
-yum --enablerepo=base -y install mc xinetd git
+yum --enablerepo=base -y install mc xinetd 
 echo
 read -rsp $'Has this Host IPMI [y/n] ?\n' -n1 IPMI
 
