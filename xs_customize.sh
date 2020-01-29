34d9c62167bbdcb5bcb18d5fcf5f74c9cf57758d
Your Name <you@example.com>
2020-01-29 20:39:50 +0100

    Customize jetzt mit besser UI Plugins check_mk mit eigenem Skript 	new file:   check_mk_install_my_plugins 	renamed:    check_mk_local_plugin_get_linux_version -> check_plugins/get_linux_version 	modified:   xs_customize.sh

--- xs_customize.sh
+++ xs_customize.sh
@@ -1,128 +1,206 @@
 #!/bin/bash
+function banner { echo -e "\n#########################################################\n"; }
+function red { echo -e "\e[31m"; }
+function nor { echo -e "\e[0m"; }
+function continue { read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1 ; }
 
-#ROT
-echo -e "\e[31mInstalling mc and xinted per yum\e[0m"
-# ROT AN     echo -e "\e[31m
-# ROT AUS    \e[0m"
-#ROT AUS      echo -e "\e[0m"
+
+
+red 
+banner
+echo "YUM: Installing mc and xinted "
+banner
+nor
+
 yum --enablerepo=base -y install mc xinetd git
 
-echo -e "\e[31mTesting, if IPMI isavailible:\e[0m"
+
+
+
+
+
+
+
+red
+banner
+echo "TEST: is IPMI availible ?"
+banner
+nor
+
 dmidecode --type 38 | grep -A 30 IPMI || echo NO IPMI detected !
 
-echo -e "\e[31m"
-read -rsp $'Has this Host IPMI [y/n] ?\n' -n1 IPMI
- echo -e "\e[0m"
+red
+banner
+read -rsp $'Check output above. \n Has this Host IPMI [y/n] ?\n' -n1 IPMI
+banner
+nor
+
 if [ $IPMI == "y" ]; then
+	
+	echo "Loading IPMI Modules.."
+	# load modules for ipmi
+	modprobe ipmi_si
+	modprobe ipmi_devintf
 
-# load modules for ipmi
-modprobe ipmi_si
-modprobe ipmi_devintf
+	# activate ipmi-modules permanently on host boot
 
-# activate ipmi-modules permanently on host boot
+	echo "modprobe ipmi_si" > /etc/sysconfig/modules/ipmi_si.modules
+	echo "modprobe ipmi_devintf" > /etc/sysconfig/modules/ipmi_devintf.modules
+	chmod +x /etc/sysconfig/modules/ipmi_si.modules
+	chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
 
+	echo "... done!"
+	red
+	banner 
+	continue
+	banner 
+	nor
+	
+	
+	# test ipmi sensors
 
-
-echo "modprobe ipmi_si" > /etc/sysconfig/modules/ipmi_si.modules
-echo "modprobe ipmi_devintf" > /etc/sysconfig/modules/ipmi_devintf.modules
-chmod +x /etc/sysconfig/modules/ipmi_si.modules
-chmod +x /etc/sysconfig/modules/ipmi_devintf.modules
-
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
-# test ipmi sensors
-
-ipmitool sensor
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
+	ipmitool sensor
+	red
+	echo "Check output above. Any sensors visible ?"
+	continue
+	nor
+else
+	echo "NO IPMI install"
 fi
 
+
+
+red
+banner
+echo "INSTALL AGENT: "
 read -rsp $'Is the OMD Host reachable per ssh ? [y/n] ?\n' -n1 OMDIP
+banner
+nor
 
 
 if [ $OMDIP == "y" ]; then
 
-# install check_mk
-echo -e "\e[31m"
-read -rsp $'Enter IP of Check_mk host : ' IP
-echo -e "\e[0m"
-scp root@$IP:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
-scp root@$IP:/omd/versions/default/share/check_mk/agents/cfg_examples/xinetd.conf /etc/xinetd.d/check_mk
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
+	echo "CHECK_MK: installing per scp"
+	red
+	read -rsp $'Enter IP of Check_mk host : ' IP
+	nor
+	echo "..copying.."
+	scp root@$IP:/omd/versions/default/share/check_mk/agents/check_mk_agent.linux /usr/bin/check_mk_agent
+	scp root@$IP:/omd/versions/default/share/check_mk/agents/cfg_examples/xinetd.conf /etc/xinetd.d/check_mk
+	red
+	continue
+	nor
+
+
 else
 
-echo. 
-echo Host IPs:
-ifconfig 
-echo -e "\e[31m"
-echo "So, you should use NOW script push_check_mk_agent on OMD HOst to push on this machine!"
-echo -e "\e[0m"
+	echo "CHECK_MK: installing per push"
+	red 
+	banner
+	echo "STOP , you should use NOW the script push_check_mk_agent on OMD Host to push to this machine!"
+	nor 
+	banner
+	echo IPs of this host:
+	ifconfig | grep inet
+	banner
+	red
+	continue
+	
+	nor
+	
 fi
 
+
+red
+banner
+echo "XINETD: Starting ?"
+continue
+banner
+nor
 # start xinetd 
+
+echo "XINETD: Starting ..."
 
 systemctl enable xinetd.service
 systemctl restart xinetd.service
+echo "...done"
 
+red 
+banner
+echo "NETSTAT: Is check_mk listening on Port 6556 (ipv4) ?"
+banner
+nor
 netstat -lnp | grep 6556
 
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
-telnet localhost 6556
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
+red
+banner
+echo "TEST: is check_mk responding on localhost:6556 (ipv4)?"
+continue
+
+banner
+echo "last 10 lines of output: "
+nor
+telnet localhost 6556 | tail
+
+
+red
+banner
+echo "FIREWALL: Open now Port 6556 for check_mk:"
+nor
 
 # make firewall exceptions
 echo "Add into /etc/sysconfig/iptables : \n"
-echo -e "\e[0m"
+nor
 echo " -A RH-Firewall-1-INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 6556 -j ACCEPT "
 echo
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
+echo " Use this line above for COPY PASTE"
+red
+continue
+nor
+
 nano /etc/sysconfig/iptables
 
-
-# start xinetd 
+echo "XINETD: Now starting..."
 
 systemctl enable xinetd.service
 systemctl restart xinetd.service
 
+echo "XINETD: started!"
+banner
+echo "IPTABLES: restarting"
 
 service iptables restart
 service iptables reload 
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
-telnet localhost 6556
-echo
-echo -e "\e[31m"
-read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
-ifconfig 
-echo -e "\e[31m"
+
+echo "IPTABLES: done"
+red
+continue
+nor
+
+
+ifconfig | grep inet
+red
+
+echo "TEST: is check_mk responding on localhost:6556 (ipv4)?"
+echo "See IP above!"
 read -rsp $'Enter IP of this host : ' IP
-echo -e "\e[0m"
-telnet $IP 6556
+
+nor
+
+echo "TELNET  $IP 6556"
+echo "last 10 lines of output: "
+
+
+
+telnet $IP 6556 | tail 
 
 echo 
-echo
 
 
 
-./check_mk_get_linux_version_install
-
-
-
-echo -e "\e[31m"
+red
 read -rsp $'Should we make mount point for /snapshots  [y /n ]\n' -n1 SNAP
-echo -e "\e[0m"
+nor
 if [ $SNAP == "y" ]; then
 
 echo " /dev/disk/by-path/ : "
@@ -147,9 +225,9 @@ echo
 
 echo
 echo
-echo -e "\e[31m"
+red
 read -rsp $'Copy UUID from above for Pasting in /etc/fstab ...\n' -n1
-echo -e "\e[0m"
+nor
 # mkdir Backup mount
 mkdir /snapshots
 
@@ -162,7 +240,7 @@ nano /etc/fstab
 mount /snapshots
 
 read -rsp $'Press key to continue OR CTRL-C to cancel...\n' -n1
-echo -e "\e[0m"
+nor
 fi
 
 
